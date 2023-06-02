75fcfb2a717b1c4267193fb8f8893adea3ea58e6
Othmane berhi <omaxnoti@gmail.com>
2023-06-02 00:11:33 +0000

    CHANGES

--- src/main/java/com/ensah/schoolmanagementsystem/controller/StudentController.java
+++ src/main/java/com/ensah/schoolmanagementsystem/controller/StudentController.java
@@ -1,13 +1,16 @@
 package com.ensah.schoolmanagementsystem.controller;
 
 import com.ensah.schoolmanagementsystem.bo.Student;
+import com.ensah.schoolmanagementsystem.excpetion.NotFoundException;
 import com.ensah.schoolmanagementsystem.service.IStudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/admin")
 @Controller
@@ -23,5 +26,18 @@ public class StudentController {
         List<Student> studentList = studentService.getAllStudentsByOrderByLastName();
         model.addAttribute("students",studentList);
         return "pages/admin/students";
+    }
+    @GetMapping("/students/{id}")
+    public String showStudentById(@PathVariable("id") Long id, Model model){
+        Optional<Student> student = studentService.getStudentById(id);
+        if(student.isEmpty()){
+            throw new NotFoundException("Student not found");
+        }
+        model.addAttribute("student",student.get());
+        return "pages/admin/student";
+    }
+    @GetMapping("/test")
+    public String test(){
+        return "pages/admin/student";
     }
 }
